829ee2a45fa6d6aab5acd807e2ad5e307973c305
LupusVII <jln.thirion@gmail.com>
2022-12-15 10:14:50 +0100

    Adjust theme config

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+	'spacing': '2rem',
+	'colors' : {
+		'bluetheme': '#1d366f',
+		'greentheme': '#08be75',
+	},
+	'primary': 'colors(bluetheme)',
+	'secondary': 'colors(greentheme)',
+	'header': {
+		'background': 'primary',
+		'font-color': 'colors(white)',
+	},
+	'container': {
+		'xl' 	: '1140px',
+		'lg' 	: '960px',
+	},
+}
